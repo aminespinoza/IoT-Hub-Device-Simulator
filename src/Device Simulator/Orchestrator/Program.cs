70e2148282947f07ef5dc334a6589322f0acfc03
Amin Espinoza <doomsday5@hotmail.com>
2018-05-03 00:01:43 -0700

    Commit 1
    Listo el primer boceto de la aplicación, la documentación completa en el
    siguiente commit.

---
+++ src/Device Simulator/Orchestrator/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Orchestrator
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        //static string connectionString = "HostName=hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=";
+
+        static void Main(string[] args)
+        {
+            int deviceCount = Convert.ToInt16(args[0]);
+            registryManager = RegistryManager.CreateFromConnectionString(args[1]);
+
+            //string str_directory = Environment.CurrentDirectory.ToString();
+            for (int i = 0; i < deviceCount; i++)
+            {
+                string myDevice = String.Format("device00{0}", i);
+
+                Task<string> callTask = Task.Run(() => AddDeviceAsync(myDevice));
+                callTask.Wait();
+                string deviceKey = callTask.Result;
+
+                var proc = new Process();
+                proc.StartInfo.UseShellExecute = true;
+                proc.StartInfo.FileName = "..\\..\\..\\SimulatedDevice\\bin\\Debug\\DeviceSimulatorStd.exe";
+                proc.StartInfo.Arguments = String.Format("syncHub.azure-devices.net {0} {1}", myDevice, deviceKey);
+                proc.Start();
+            }
+            Console.WriteLine("All set");
+            Console.ReadLine();
+        }
+
+        private static async Task<string> AddDeviceAsync(string deviceName)
+        {
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceName));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceName);
+            }
+            Console.WriteLine("Generated new device named: {0}", deviceName);
+
+            return device.Authentication.SymmetricKey.PrimaryKey;
+        }
+    }
+}
---
+++ src/Device Simulator/SimulatedDevice/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Sorry, no parameters");
+            }
+            else
+            {
+                SendTelemetryData(args[0], args[1], args[2]);
+            }
+            Console.ReadLine();
+        }
+
+        private static async void SendTelemetryData(string iotHubUri, string deviceId, string deviceKey)
+        {
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey));
+
+            double avgTemperature = 21;
+            double avgHumidity = 35;
+            double avgPower = 50;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double humidity = Math.Round((avgHumidity + rand.NextDouble() * 4 - 2), 2);
+                double temperature = Math.Round((avgTemperature + rand.NextDouble() * 4 - 2), 2);
+                double power = Math.Round((avgPower + rand.NextDouble()), 2);
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = deviceId,
+                    humidity = humidity,
+                    temperature = temperature,
+                    power = power,
+                    date = DateTime.Now
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Thread.Sleep(1000);
+                Console.WriteLine(messageString);
+            }
+        }
+    }
+}
